399dd1325414131d69ce1dbc5d571d688cdbca4a
Sébastien Forestier <sebastien.forestier@inria.fr>
2017-03-28 12:56:21 +0200

    learning: remove bootstrap check

--- ros/nips2016/src/nips2016/learning/core/sensorimotor_model.py
+++ ros/nips2016/src/nips2016/learning/core/sensorimotor_model.py
@@ -14,16 +14,11 @@ class DemonstrableNN(NonParametric):
         
     def save(self):
         return [[self.model.imodel.fmodel.dataset.get_x(i) for i in range(len(self.model.imodel.fmodel.dataset))],
-                [self.model.imodel.fmodel.dataset.get_y(i) for i in range(len(self.model.imodel.fmodel.dataset))],
-                self.bootstrapped_s]
+                [self.model.imodel.fmodel.dataset.get_y(i) for i in range(len(self.model.imodel.fmodel.dataset))]]
     
     def forward(self, data, iteration):
         self.model.imodel.fmodel.dataset.add_xy_batch(data[0][:iteration], data[1][:iteration])
         self.t = len(self.model.imodel.fmodel.dataset)
-        if len(data) > 2:
-            self.bootstrapped_s = data[2]
-        else:
-            self.bootstrapped_s = True
 
     def infer(self, in_dims, out_dims, x):
         if self.t < max(self.model.imodel.fmodel.k, self.model.imodel.k):
@@ -33,24 +28,19 @@ class DemonstrableNN(NonParametric):
             return array(self.model.predict_effect(tuple(x))), -1
 
         elif in_dims == self.s_dims and out_dims == self.m_dims:  # inverse
-            if not self.bootstrapped_s:
-                # If only one distinct point has been observed in the sensory space, then we output a random motor command
-                return rand_bounds(np.array([self.m_mins,
-                                             self.m_maxs]))[0], -1
-            else:
-                self.mean_explore = array(self.model.infer_order(tuple(x)))
-                idx = -1
-                # Check if nearest s was demonstrated
-                if np.linalg.norm(self.mean_explore) == 0:
-                    idx = self.model.imodel.fmodel.dataset.nn_y(x)[1][0]
-                    #print "demonstrated idx", idx
-                if self.mode == 'explore':
-                    r = self.mean_explore
-                    r[self.sigma_expl > 0] = np.random.normal(r[self.sigma_expl > 0], self.sigma_expl[self.sigma_expl > 0])
-                    res = bounds_min_max(r, self.m_mins, self.m_maxs)
-                    return res, idx
-                else:  # exploit'
-                    return array(self.model.infer_order(tuple(x))), idx
+            self.mean_explore = array(self.model.infer_order(tuple(x)))
+            idx = -1
+            # Check if nearest s was demonstrated
+            if np.linalg.norm(self.mean_explore) == 0:
+                idx = self.model.imodel.fmodel.dataset.nn_y(x)[1][0]
+                #print "demonstrated idx", idx
+            if self.mode == 'explore':
+                r = self.mean_explore
+                r[self.sigma_expl > 0] = np.random.normal(r[self.sigma_expl > 0], self.sigma_expl[self.sigma_expl > 0])
+                res = bounds_min_max(r, self.m_mins, self.m_maxs)
+                return res, idx
+            else:  # exploit'
+                return array(self.model.infer_order(tuple(x))), idx
 
         else:
             raise NotImplementedError
@@ -76,6 +66,3 @@ class DemonstrableNN(NonParametric):
     def update(self, m, s):
         self.model.add_xy(tuple(m), tuple(s))
         self.t += 1
-        if not self.bootstrapped_s and self.t > 1:
-            if not (list(s[2:]) == list(self.model.imodel.fmodel.dataset.get_y(0)[2:])):
-                self.bootstrapped_s = True
